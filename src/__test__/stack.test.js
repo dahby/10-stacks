40b23a29a9d82af2e9e1aeb24de348252a9fb480
dahby <dstoll243@gmail.com>
2018-04-27 16:29:21 -0700

    first tests passing

--- src/__test__/stack.test.js
+++ src/__test__/stack.test.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const Stack = require('../lib/stack');
+
+describe('stack.js testing', () => {
+  describe('#push', () => {
+    test('Should return value of newly pushed nodes', () => {
+      const stack = new Stack();
+      stack.push(1);
+      expect(stack._storage.head.value).toEqual(1);
+      stack.push(2);
+      stack.push(3);
+      expect(stack._storage.head.next.value).toEqual(2);
+      expect(stack._storage.head.next.next.value).toEqual(3);
+    });
+  });
+});
